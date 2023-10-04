cf1f0dbc2c36f8711c7cbba959e6fba260c8e6a0
Bobob0b <github@mzx.at>
2023-10-05 00:16:31 +0200

    weitere übungen

---
+++ fundamentals part 03.js
@@ -0,0 +1,17 @@
+function add7(number) {
+return number + 7
+}
+
+function multiply(one, two){
+    return one * two
+}
+
+function capitalize(text) {
+    text = text.toLowerCase();
+    return text.replace(text[0],text[0].toUpperCase())
+
+}
+
+function lastLetter(text){
+    return text[text.length-1]
+}
